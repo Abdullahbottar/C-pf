44959c33239afe4261f03b6bbf418f17fe6e70e1
abdullahbottar <abdullahbottar921@gmail.com>
2024-01-12 01:18:38 +0500

    4nd commit

--- homework/22l-6913 Q6B .cpp
+++ homework/22l-6913 Q6B .cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
 	cout << "Enter motor-cycle:" << endl;
+	cout << "Enter motor-cycle:" << endl;
 	string v;
 	cout << "Enter 1 motor-cycle:" << endl;
 	cout << "Enter 2 car:" << endl;
