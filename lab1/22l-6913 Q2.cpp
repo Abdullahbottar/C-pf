372409d614ddbac62ecccb29ffaa94eceaba6ad3
Abdullahbottar <abdullahbottar921@gmail.com>
2024-10-09 22:58:26 +0500

    1st commit

--- lab1/22l-6913 Q2.cpp
+++ lab1/22l-6913 Q2.cpp
@@ -1,27 +1,27 @@
-//#include <iostream>
-//using namespace std;
-//int main()
-//{
-//	int s;
-//	char c;
-//	float f;
-//	double w;
-//	cout << "enter the vaue of int s:";
-//	cin >> s;
-//	cout << endl;
-//	cout << "enter the value of char c:";
-//	cin >> c;
-//	cout << endl;
-//	cout << "enter the value of float f:";
-//	cin >> f;
-//	cout << endl;
-//	cout << "enter the value of double w:";
-//	cin >> w;
-//	cout << endl;
-//	cout << "the size of int value is:" << sizeof(s) << " bytes." << endl;
-//	cout << "the size of float value is:" << sizeof(c) << " bytes." << endl;
-//	cout << "the size of char value is:" << sizeof(f) << " bytes." << endl;
-//	cout << "the size of double value is:" << sizeof(w) << " bytes." << endl;
-//	system("pause");
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+int main()
+{
+	int s;
+	char c;
+	float f;
+	double w;
+	cout << "enter the vaue of int s:";
+	cin >> s;
+	cout << endl;
+	cout << "enter the value of char c:";
+	cin >> c;
+	cout << endl;
+	cout << "enter the value of float f:";
+	cin >> f;
+	cout << endl;
+	cout << "enter the value of double w:";
+	cin >> w;
+	cout << endl;
+	cout << "the size of int value is:" << sizeof(s) << " bytes." << endl;
+	cout << "the size of float value is:" << sizeof(c) << " bytes." << endl;
+	cout << "the size of char value is:" << sizeof(f) << " bytes." << endl;
+	cout << "the size of double value is:" << sizeof(w) << " bytes." << endl;
+	system("pause");
+	return 0;
+}
